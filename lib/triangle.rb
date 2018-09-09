708c9f01159dd4dcfe755971d7c483bd825e6e75
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 11:13:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,12 +27,10 @@ class Triangle
 
   def validTriangle?
     if !@side1 || !@side2 || !@side3
-      raise TriangleError
       false
     elsif  @side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2
-      raise TriangleError
+      false
     elsif @side1<= 0 || @side2<= 0 || @side3<= 0
-      raise TriangleError
       false
     end
     true

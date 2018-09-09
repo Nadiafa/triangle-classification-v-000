3a6a3f056833344e097aa0b41a6d75035d9e6ecc
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 10:16:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if validTriangle?
+      if @side1 == @side2 && @side1 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        :isosceles
+      else 
+        :scalene
+      end
+    else
+      begin 
+        raise TriangleError
+      rescue TriangleError => error
+      puts error.message
+      end
+    end
+  end
+
+###### IS VALID TRIANGLE!!!
+  def validTriangle?
+    if (@side1+@side2>@side3 || @side2+@side3>@side1 || @side1+@side3>@side2) && ((@side1|| @side2 || @side3) > 0)
+      true 
+    else
+      false 
+    end
+  end
+###### IS VALID TRIANGLE!!!
+
+  class TriangleError < StandardError
+	    def message 
+	      "The triangle is invalid."
+	    end
+	  end
+
 end
+

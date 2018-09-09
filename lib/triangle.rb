db9624c9aeb5de01bc0766f71bb49df81b9d8282
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 11:34:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,11 +26,7 @@ class Triangle
     elsif side1 > 0 || side2 > 0 || side3 > 0
       true
     else
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
     end
   end
 

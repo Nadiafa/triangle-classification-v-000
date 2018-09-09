be5ebe0cfe4a29ac8a681d9806bfaef0da3c6bf9
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 11:37:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
 
   def validTriangle
     
-    if  side1+side2>side3 && side2+side3>side1 && side1+side3>side2
+    if  side1+side2>side3 || side2+side3>side1 || side1+side3>side2
       true
     elsif side1 > 0 || side2 > 0 || side3 > 0
       true

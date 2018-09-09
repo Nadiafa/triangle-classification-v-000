873baed377ef256a00f23afa99b1670b41235dc2
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 10:27:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -30,10 +30,10 @@ class Triangle
   def validTriangle?
     if @side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2
       false
-    elsif (@side1 && @side2 && @side3) <= 0
-    
+    elsif @side1<= 0 || @side2<= 0 || @side3<= 0
+      false
     else
-      false 
+      true 
     end
   end
 ###### IS VALID TRIANGLE!!!

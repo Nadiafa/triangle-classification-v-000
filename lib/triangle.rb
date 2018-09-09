bfbda3cee403990f71c662084165e9e41cdab1fe
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 11:34:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
     
     if  side1+side2>side3 || side2+side3>side1 || side1+side3>side2
       true
-    elsif side1 < 0 || side2 < 0 || side3 < 0
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
       true
     else
       raise TriangleError

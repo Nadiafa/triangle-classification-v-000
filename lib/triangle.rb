49ce9fde172983e4cf5eb5d98e2ecc4c26f62ef8
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-09 11:48:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,13 +20,11 @@ class Triangle
   end
 
   def validTriangle
-      triangle_valid = nil
-    if side1+side2>side3 || side2+side3>side1 || side1+side3>side2
-      triangle_valid =true
-    elsif side1 > 0 || side2 > 0 || side3 > 0
-      triangle_valid =true
-    else
       triangle_valid = false
+    if side1+side2>side3 || side2+side3>side1 || side1+side3>side2
+      triangle_valid = true
+    elsif side1 > 0 || side2 > 0 || side3 > 0
+      triangle_valid = true
     end
     raise TriangleError if triangle_valid == false
   end
